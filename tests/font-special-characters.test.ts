97d236a65c0b6b6fb2b5c3281890ad58d42e4cd9
techmannih <manishchaudharyttt@gmail.com>
2025-12-19 18:44:10 +0530

    Improve plus and subtract sign

---
+++ tests/font-special-characters.test.ts
@@ -0,0 +1,16 @@
+import { expect, test } from "bun:test"
+import { svgAlphabet } from "../index"
+import { renderTextToPng } from "./helpers/render-text-to-png"
+
+test("renders all special characters", async () => {
+  // Extract all special characters (non-alphanumeric) from svgAlphabet
+  const specialCharacters = Object.keys(svgAlphabet)
+    .filter((char) => !/[a-zA-Z0-9]/.test(char))
+    .sort()
+    .join("")
+  console.log(specialCharacters)
+  const pngBuffer = renderTextToPng(specialCharacters, {
+    filename: "font-special-characters.png",
+  })
+  expect(pngBuffer.length).toBeGreaterThan(1000)
+})
